f747940f14989179e26d10080e84157fd712af7a
Pritesh Bhavsar <bhavsarpritesh.mca@gmail.com>
2022-01-24 12:33:40 +0530

    viewdata demo

--- WebApplication1/Controllers/CategoryController.cs
+++ WebApplication1/Controllers/CategoryController.cs
@@ -10,6 +10,7 @@ namespace WebApplication1.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["var1"] = "pritesh";
             return View(); 
         }
     }
