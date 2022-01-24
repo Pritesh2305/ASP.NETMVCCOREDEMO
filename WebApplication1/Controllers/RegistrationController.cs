edc529984374aba24e01d70d0e27d453bf881083
Pritesh Bhavsar <bhavsarpritesh.mca@gmail.com>
2022-01-24 11:19:09 +0530

    Initial Commit

---
+++ WebApplication1/Controllers/RegistrationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index(int id,string name)
+        {
+            int idget = id;
+            string name1 = name;
+            return View();
+        }
+
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
